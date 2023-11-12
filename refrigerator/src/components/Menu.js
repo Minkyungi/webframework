476407d7d539db48055fc916fad0c45973a62dc0
songhm7 <1991203@hansung.ac.kr>
2023-11-12 18:55:17 +0900

    필터와 검색을 별도 컴포넌트로 분리하여 코드 가독성과 재사용성을 향상

--- refrigerator/src/components/Menu.js
+++ refrigerator/src/components/Menu.js
@@ -2,6 +2,8 @@
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import RecipeList from './RecipeList';
+import FilterSearch from './FilterSearch';
+
 
 const Menu = () => {
   const [allRecipes, setAllRecipes] = useState([]); // 전체 레시피를 저장할 상태변수
@@ -79,90 +81,18 @@ const Menu = () => {
       <h1>현재 메뉴 페이지에 위치</h1>
       <Link to="/">홈 페이지로 이동</Link>
       <br />
-      <form onSubmit={(e) => { e.preventDefault(); handleSearchFilter(); }}>
-        <input
-          type='radio'
-          name='radio1'
-          value='foodName'
-          checked={searchOption === 'foodName'}
-          onChange={() => setSearchOption('foodName')}
-        />
-        음식명
-        <input
-          type='radio'
-          name='radio1'
-          value='ingredientName'
-          checked={searchOption === 'ingredientName'}
-          onChange={()=> setSearchOption('ingredientName')}
-        />
-        재료명
-        <input
-          type="text"
-          placeholder="요리명 혹은 재료명을 입력하세요"
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <input type='submit' value='검색' />
-        <div>
-          <input
-            type="radio"
-            name="category"
-            value="all"
-            checked={selectedCategory === 'all'}
-            onChange={() => setSelectedCategory('all')}
-          /> 전체
-          <input
-            type="radio"
-            name="category"
-            value="rice"
-            checked={selectedCategory === 'rice'}
-            onChange={() => setSelectedCategory('rice')}
-          /> 밥
-          <input
-            type="radio"
-            name="category"
-            value="dessert"
-            checked={selectedCategory === 'dessert'}
-            onChange={() => setSelectedCategory('dessert')}
-          /> 디저트
-          <input
-            type="radio"
-            name="category"
-            value="noodle"
-            checked={selectedCategory === 'noodle'}
-            onChange={() => setSelectedCategory('noodle')}
-          /> 국수
-          <input
-            type="radio"
-            name="category"
-            value="soup"
-            checked={selectedCategory === 'soup'}
-            onChange={() => setSelectedCategory('soup')}
-          /> 국
-          <input
-            type="radio"
-            name="category"
-            value="sideDish"
-            checked={selectedCategory === 'sideDish'}
-            onChange={() => setSelectedCategory('sideDish')}
-          /> 간식
-        </div>
-      </form>
-      <div>
-        <label htmlFor="maxTime">최대 요리 시간:</label>
-        <input 
-          type="number" 
-          id="maxTime" 
-          value={maxTime} 
-          onChange={(e) => setMaxTime(e.target.value)}
-        />
-        <label htmlFor="maxCalories">최대 칼로리:</label>
-        <input 
-          type="number" 
-          id="maxCalories" 
-          value={maxCalories} 
-          onChange={(e) => setMaxCalories(e.target.value)}
-        />
-    </div>
+      <FilterSearch
+        setSearchOption={setSearchOption}
+        setSearchTerm={setSearchTerm}
+        setSelectedCategory={setSelectedCategory}
+        setMaxTime={setMaxTime}
+        setMaxCalories={setMaxCalories}
+        handleSearchFilter={handleSearchFilter}
+        searchOption={searchOption}
+        selectedCategory={selectedCategory}
+        maxTime={maxTime}
+        maxCalories={maxCalories}
+      />
       <RecipeList
         recipes={recipes.slice(
           (currentPage - 1) * recipesPerPage,
