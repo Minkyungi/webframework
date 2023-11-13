32c10554dcd0989e48208e5da4a506a96f221d9e
songhm7 <1991203@hansung.ac.kr>
2023-11-13 12:26:54 +0900

    모달에 카테고리 표시

--- refrigerator/src/components/RecipeModal.js
+++ refrigerator/src/components/RecipeModal.js
@@ -4,7 +4,14 @@ import '../styles/RecipeModal.css'; // CSS 파일 임포트
 
 const RecipeModal = ({ recipe, onClose }) => {
     if (!recipe) return null;
-
+    let category;
+    switch(recipe.category){
+        case("rice") : category = "밥"; break;
+        case("noodle") : category = "면"; break;
+        case("soup") : category = "국"; break;
+        case("dessert") : category = "디저트"; break;
+        case("sideDish") : category = "간식"; break;
+    }
 
     return (
         <div className="modal-backdrop">
@@ -23,6 +30,8 @@ const RecipeModal = ({ recipe, onClose }) => {
                     {recipe.f_url && <div>관련 영상: <a href={recipe.f_url}>{recipe.f_url}</a></div>}
                     <div>칼로리: {recipe.calories}</div>
                     <div>조리 시간: {recipe.f_time}</div>
+                    <div>카테고리: {category}</div>
+
                 </div>
                 <div className="modal-footer">
                     <button onClick={onClose}>닫기</button>
@@ -30,31 +39,6 @@ const RecipeModal = ({ recipe, onClose }) => {
             </div>
         </div>
     );
-    /*return (
-        <div className="modal-backdrop">
-            <div className="modal">
-                <div className="modal-content">
-                    <div className="modal-column">
-                        <h2>{recipe.f_name}</h2>
-                        <h3>재료:</h3>
-                        <ul>
-                            {recipe.f_materials.map((x, i) => <li key={i}>{x}</li>)}
-                        </ul>
-                    </div>
-                    <div className="modal-column">
-                        <h3>조리 순서:</h3>
-                        <ul>
-                            {recipe.f_make.map((x, i) => <li key={i}>{x}</li>)}
-                        </ul>
-                        <h3>조리 시간: {recipe.f_time}</h3>
-                    </div>
-                </div>
-                <div className="modal-footer">
-                    <button onClick={onClose}>닫기</button>
-                </div>
-            </div>
-        </div>
-    );*/
 };
 
 export default RecipeModal;
